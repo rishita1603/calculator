f340f5a063556de27dcba496a8b866a7a90578d0
rishita1603 <rishita1617@gmail.com>
2023-11-15 12:11:06 +0530

    docs

---
+++ script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+function removeLastDigit() {
+    let currentValue = display.value;
+    display.value = currentValue.slice(0, -1);
+}
